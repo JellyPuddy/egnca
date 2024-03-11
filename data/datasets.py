feat: Make anchor structure and loss function choosable params; add param for fourrier angle features; fix angle calculation

data/datasets.py
@@ -227,7 +227,8 @@ class SBM(InMemoryDataset):
 
 def load_pygsp_graph(
     name: str,
-    structured_seed: Optional[bool] = False,
+    anchor_coords: Optional[torch.Tensor] = None,
+    anchor_dist: Optional[float] = 0.25,
     **kwargs
 ):
     import pygsp
@@ -236,21 +237,20 @@ def load_pygsp_graph(
 
     coord_dim = coord.size(1)
 
-    if structured_seed:
+    if anchor_coords is not None:
         # Add anchor points
-        anchor_coords = get_anchor_coords(coord_dim)
         coord = torch.cat([coord, anchor_coords], dim=0)
 
     edge_index = EdgeIndex(np.stack([graph.W.tocoo().row, graph.W.tocoo().col]).astype(np.int64))
 
-    if structured_seed:
+    if anchor_coords is not None:
         # Add an edge from each anchor to each node
         anchor_edges = torch.tensor([[i, j] for i in range(graph.N) for j in range(graph.N, graph.N + coord_dim + 1)])
         # Add the reverse edges
         anchor_edges = torch.cat([anchor_edges, anchor_edges.flip(1)], dim=0)
-        # Keep only those edges with a distance smaller than 0.15
+        # Keep only those edges with a distance smaller than anchor_dist
         dist = torch.norm(coord[anchor_edges[:, 0]] - coord[anchor_edges[:, 1]], dim=-1)
-        anchor_edges = anchor_edges[dist < 0.15]
+        anchor_edges = anchor_edges[dist < anchor_dist]
         edge_index = EdgeIndex(torch.cat([edge_index, anchor_edges.T], dim=1))
 
     return coord, edge_index
@@ -259,35 +259,36 @@ def load_pygsp_graph(
 def create_cube_cloud(
     length: Optional[int] = 8,
     radius: Optional[float] = 0.15,
-    structured_seed: Optional[bool] = False
+    anchor_coords: Optional[torch.Tensor] = None,
+    anchor_dist: Optional[float] = 0.25
 ):
     values = torch.linspace(0, 1, steps=length)
     coord = torch.stack(torch.meshgrid(values, values, values, indexing='xy')).reshape(3, -1).T
     dist = torch.norm(coord - coord.unsqueeze(1), dim=-1) < radius
     dist.fill_diagonal_(0)
 
-    if structured_seed:
+    if anchor_coords is not None:
         # Add anchor points
-        anchor_coords = get_anchor_coords(3)
         coord = torch.cat([coord, anchor_coords], dim=0)
 
     edge_index = EdgeIndex(dist.nonzero().T.contiguous())
 
-    if structured_seed:
+    if anchor_coords is not None:
         # Add an edge from each anchor to each node
         anchor_edges = torch.tensor([[i, j] for i in range(coord.size(0) - 4) for j in range(coord.size(0) - 4, coord.size(0))])
         # Add the reverse edges
         anchor_edges = torch.cat([anchor_edges, anchor_edges.flip(1)], dim=0)
-        # Keep only those edges with a distance smaller than radius
+        # Keep only those edges with a distance smaller than anchor_dist
         dist = torch.norm(coord[anchor_edges[:, 0]] - coord[anchor_edges[:, 1]], dim=-1)
-        anchor_edges = anchor_edges[dist < radius]
+        anchor_edges = anchor_edges[dist < anchor_dist]
         edge_index = EdgeIndex(torch.cat([edge_index, anchor_edges.T], dim=1))
 
     return coord, edge_index
 
 
 def create_pyramid(
-    structured_seed: Optional[bool] = False
+    anchor_coords: Optional[torch.Tensor] = None,
+    anchor_dist: Optional[float] = 0.25
 ):
     values = np.linspace(0, 1, num=16)
     x, y, z = np.meshgrid(values, values, values, indexing='xy')
@@ -297,48 +298,46 @@ def create_pyramid(
     dist = np.sum((coord[:, None] - coord[None, :]) ** 2, axis=-1)
     np.fill_diagonal(dist, np.inf)
 
-    if structured_seed:
+    if anchor_coords is not None:
         # Add anchor points
-        anchor_coords = get_anchor_coords(3)
         coord = np.vstack([coord, anchor_coords])
 
     edge_index = np.column_stack(np.where(
         np.logical_or(dist <= 0.072, np.logical_and(0.4475 < dist, dist < 0.4490))))
     coord, edge_index = torch.Tensor(coord), EdgeIndex(np.ascontiguousarray(edge_index.T, dtype=np.int64))
 
-    if structured_seed:
+    if anchor_coords is not None:
         # Add an edge from each anchor to each node
         anchor_edges = torch.tensor([[i, j] for i in range(coord.size(0) - 4) for j in range(coord.size(0) - 4, coord.size(0))])
         # Add the reverse edges
         anchor_edges = torch.cat([anchor_edges, anchor_edges.flip(1)], dim=0)
-        # Keep only those edges with a distance smaller than 0.15
+        # Keep only those edges with a distance smaller than anchor_dist
         dist = torch.norm(coord[anchor_edges[:, 0]] - coord[anchor_edges[:, 1]], dim=-1)
-        anchor_edges = anchor_edges[dist < 0.15]
+        anchor_edges = anchor_edges[dist < anchor_dist]
         edge_index = EdgeIndex(torch.cat([edge_index, anchor_edges.T], dim=1))
 
     return coord, edge_index
 
 
-def create_line(n_points=64, structured_seed=False):
+def create_line(n_points=64, anchor_coords=None, anchor_dist=0.25):
     coord = torch.linspace(0, 1, steps=n_points).unsqueeze(1).repeat(1, 2)
     coord = (coord - coord.mean(0)) / coord.std(0)
     dist = ((coord.unsqueeze(1) - coord.unsqueeze(0)) ** 2).sum(dim=-1).fill_diagonal_(torch.inf)
 
-    if structured_seed:
+    if anchor_coords is not None:
         # Add anchor points
-        anchor_coords = get_anchor_coords(2)
         coord = torch.cat([coord, anchor_coords], dim=0)
 
     edge_index = EdgeIndex(torch.argwhere(dist <= dist.min() + 0.001).T.contiguous())
 
-    if structured_seed:
+    if anchor_coords is not None:
         # Add an edge from each anchor to each node
         anchor_edges = torch.tensor([[i, j] for i in range(coord.size(0) - 3) for j in range(coord.size(0) - 3, coord.size(0))])
         # Add the reverse edges
         anchor_edges = torch.cat([anchor_edges, anchor_edges.flip(1)], dim=0)
-        # Keep only those edges with a distance smaller than 0.15
+        # Keep only those edges with a distance smaller than anchor_dist
         dist = torch.norm(coord[anchor_edges[:, 0]] - coord[anchor_edges[:, 1]], dim=-1)
-        anchor_edges = anchor_edges[dist < 0.15]
+        anchor_edges = anchor_edges[dist < anchor_dist]
         edge_index = EdgeIndex(torch.cat([edge_index, anchor_edges.T], dim=1))
 
     return coord, edge_index
@@ -346,21 +345,27 @@ def create_line(n_points=64, structured_seed=False):
 
 def get_geometric_graph(
     name: str,
-    structured_seed: Optional[bool] = False,
+    anchor_structure: Optional[torch.Tensor] = None,
+    anchor_dist: Optional[float] = 0.25,
     **kwargs
 ):
+    dimensions = 2 if name in ['Grid2d', 'Line'] else 3
+    anchor_coords = get_anchor_coords(anchor_structure, dimensions)
+
     if name == 'Line':
-        coord, edge_index = create_line(structured_seed=structured_seed)
+        coord, edge_index = create_line(anchor_coords=anchor_coords, anchor_dist=anchor_dist)
     elif name == 'Cube':
-        coord, edge_index = create_cube_cloud(structured_seed=structured_seed)
+        coord, edge_index = create_cube_cloud(anchor_coords=anchor_coords, anchor_dist=anchor_dist)
     elif name == 'Pyramid':
-        coord, edge_index = create_pyramid(structured_seed=structured_seed)
+        coord, edge_index = create_pyramid(anchor_coords=anchor_coords, anchor_dist=anchor_dist)
     elif name in ['Bunny', 'Grid2d', 'Torus']:
-        coord, edge_index = load_pygsp_graph(name, structured_seed, **kwargs)
+        coord, edge_index = load_pygsp_graph(name, anchor_coords, anchor_dist=anchor_dist, **kwargs)
     else:
+        if anchor_coords is not None:
+            raise NotImplementedError('Structured seeds are not (yet) supported for this graph.')
         coord, edge_index = torch.load('./data/clouds/%s.pt' % name)
         edge_index = EdgeIndex(edge_index)
-    return coord, edge_index
+    return coord, edge_index, anchor_coords
 
 
 class GeometricGraphDataset(Dataset):

egnn.py
@@ -63,7 +63,8 @@ class EGC(nn.Module):
         aggr_coord: Optional[str] = 'mean',
         aggr_hidden:  Optional[str] = 'sum',
         has_coord_act: Optional[bool] = False,
-        use_angles: Optional[bool] = False
+        use_angles: Optional[bool] = False,
+        ffa: Optional[bool] = False
     ):
         super(EGC, self).__init__()
         assert aggr_coord == 'mean' or aggr_coord == 'sum'
@@ -84,8 +85,10 @@ class EGC(nn.Module):
         self.aggr_hidden = aggr_hidden
         self.has_coord_act = has_coord_act
         self.use_angles = use_angles
+        self.ffa = ffa
 
         expressiveness_dim = message_dim if use_angles else 0
+        angle_dim = 8 if ffa else 1
 
         act = {'tanh': nn.Tanh(), 'lrelu': nn.LeakyReLU(), 'silu': nn.SiLU()}[act_name]
 
@@ -98,7 +101,7 @@ class EGC(nn.Module):
 
         if use_angles:
             self.edge_mlp2 = nn.Sequential(
-                nn.Linear(node_dim + node_dim + edge_attr_dim + 8, message_dim),
+                nn.Linear(node_dim + node_dim + edge_attr_dim + angle_dim, message_dim),
                 act,
                 nn.Linear(message_dim, message_dim),
                 act
@@ -222,7 +225,10 @@ class EGC(nn.Module):
     ):
         angle, (idx_i, idx_j, idx_k) = calculate_angles(coord, edge_index, node_feat.size(0), True)
 
-        transformed_angle = get_fourrier_features(angle)
+        if self.ffa:
+            transformed_angle = get_fourrier_features(angle)
+        else:
+            transformed_angle = angle.unsqueeze(1)
         edge_feat2 = torch.cat([node_feat[idx_j], node_feat[idx_k], transformed_angle], dim=1)
         out = self.edge_mlp2(edge_feat2)
         out_agg = aggregated_sum_2_indices(out, idx_i, idx_j, coord_radial.size(0), mean=self.aggr_coord == 'mean')

models.py
@@ -62,7 +62,9 @@ class EncoderEGNCA(nn.Module):
         structured_seed: Optional[bool] = False,
         scale: Optional[float] = 1.0,
         update_anchor_feat: Optional[bool] = False,
-        anchor_feat: Optional[torch.Tensor] = None
+        anchor_feat: Optional[torch.Tensor] = None,
+        anchor_coords: Optional[torch.Tensor] = None,
+        ffa: Optional[bool] = False
     ):
         super(EncoderEGNCA, self).__init__()
         assert norm_type is None or norm_type == 'nn' or norm_type == 'pn'
@@ -80,6 +82,8 @@ class EncoderEGNCA(nn.Module):
         self.scale = scale
         self.update_anchor_feat = update_anchor_feat
         self.anchor_feat = anchor_feat
+        self.anchor_coords = anchor_coords
+        self.ffa = ffa
 
         if norm_type == 'nn':
             self.normalise = NodeNorm(root_power=2.0 if norm_cap is None else norm_cap)
@@ -98,7 +102,8 @@ class EncoderEGNCA(nn.Module):
                 is_residual=is_residual,
                 has_attention=has_attention,
                 has_coord_act=has_coord_act,
-                use_angles=use_angles))
+                use_angles=use_angles,
+                ffa=ffa))
         self.egnn = EGNN(layers)
 
     @property
@@ -118,7 +123,7 @@ class EncoderEGNCA(nn.Module):
         coord = torch.empty(num_nodes, self.coord_dim, dtype=dtype, device=device).normal_(self.std)
 
         if self.structured_seed:
-            anchor_coords = get_anchor_coords(self.coord_dim) * self.scale
+            anchor_coords = self.anchor_coords * self.scale
             coord[-anchor_coords.size(0):] = anchor_coords
 
         return coord
@@ -135,8 +140,8 @@ class EncoderEGNCA(nn.Module):
             node_feat = torch.ones(num_nodes, self.node_dim, dtype=dtype, device=device)
         
         if self.structured_seed:
-            # Replace the last coord_dim+1 nodes with the anchor nodes
-            node_feat[-(self.coord_dim+1):] = self.anchor_feat
+            # Replace the last nodes with the anchor nodes
+            node_feat[-self.anchor_feat.size(0):] = self.anchor_feat
         return node_feat
 
     def stochastic_update(
@@ -158,18 +163,18 @@ class EncoderEGNCA(nn.Module):
             mask = (torch.rand(out_coord.size(0), 1) <= self.fire_rate).byte().to(in_coord.device)
             # Make the mask 0 for the anchor nodes
             if self.structured_seed and not self.update_anchor_feat:
-                mask[-(self.coord_dim+1):] = 0
+                mask[-self.anchor_feat.size(0):] = 0
             out_node_feat = (out_node_feat * mask) + (in_node_feat * (1 - mask))
             if self.structured_seed:
-                mask[-(self.coord_dim+1):] = 0
+                mask[-self.anchor_feat.size(0):] = 0
             out_coord = (out_coord * mask) + (in_coord * (1 - mask))
         elif self.structured_seed:
-            out_coord[-(self.coord_dim+1):] = in_coord[-(self.coord_dim+1):]
-            anchor_mask = torch.zeros(self.coord_dim+1, self.node_dim).byte().to(in_coord.device)
+            out_coord[-self.anchor_feat.size(0):] = in_coord[-self.anchor_feat.size(0):]
+            anchor_mask = torch.zeros(self.anchor_feat.size(0), self.node_dim).byte().to(in_coord.device)
             # Possibly update the anchor features
             if self.update_anchor_feat:
                 anchor_mask += 1
-            out_node_feat[-(self.coord_dim+1):] = (out_node_feat[-(self.coord_dim+1):] * anchor_mask) + (in_node_feat[-(self.coord_dim+1):] * (1 - anchor_mask))
+            out_node_feat[-self.anchor_feat.size(0):] = (out_node_feat[-self.anchor_feat.size(0):] * anchor_mask) + (in_node_feat[-self.anchor_feat.size(0):] * (1 - anchor_mask))
         assert not torch.isnan(out_node_feat).any()
         assert not torch.isnan(out_coord).any()
         return out_coord, out_node_feat
@@ -213,10 +218,11 @@ class FixedTargetGAE(pl.LightningModule):
         super().__init__()
 
         self.structured_seed = args.structured_seed if 'structured_seed' in args else False
+        self.anchor_structure = args.anchor_structure if 'anchor_structure' in args else None
 
         # load target geometric graph as model attribute
         from data.datasets import get_geometric_graph
-        target_coord, edge_index = get_geometric_graph(args.dataset, structured_seed=self.structured_seed)
+        target_coord, edge_index, anchor_coords = get_geometric_graph(args.dataset, anchor_structure=self.anchor_structure)
         self.register_buffer('target_coord', target_coord * args.scale)
         self.register_buffer('edge_index', edge_index)
 
@@ -228,11 +234,12 @@ class FixedTargetGAE(pl.LightningModule):
         self.edge_distance = args.edge_distance if 'edge_distance' in args else 0.15
         self.edge_num = args.edge_num if 'edge_num' in args else None
         self.update_anchor_feat = args.update_anchor_feat if 'update_anchor_feat' in args else False
-
-        if self.structured_seed:
+        self.loss_fn = args.loss if 'loss' in args else 'mse'
+        self.ffa = args.fourrier_feat_angles if 'fourrier_feat_angles' in args else False
+        if self.structured_seed and anchor_coords is not None:
+            self.register_buffer('anchor_coords', anchor_coords * args.scale)
             # Initialize features of anchor nodes. The features are learnable parameters.
-            anchor_feat = torch.nn.Parameter(torch.empty(self.target_coord.size(1) + 1, args.node_dim, dtype=torch.float32).normal_(args.std))
-            self.register_parameter('anchor_feat', anchor_feat)
+            self.register_parameter('anchor_feat', torch.nn.Parameter(torch.empty(self.anchor_coords.size(0), args.node_dim, dtype=torch.float32).normal_(args.std)))
 
         self.encoder = EncoderEGNCA(
             coord_dim=self.target_coord.size(1),
@@ -254,7 +261,9 @@ class FixedTargetGAE(pl.LightningModule):
             structured_seed=self.structured_seed,
             scale=args.scale,
             update_anchor_feat=self.update_anchor_feat,
-            anchor_feat=self.anchor_feat if self.structured_seed else None)
+            anchor_feat=self.anchor_feat if self.structured_seed else None,
+            anchor_coords=self.anchor_coords if self.structured_seed else None,
+            ffa=self.ffa)
 
         self.pool = GaussianSeedPool(
             pool_size=args.pool_size,
@@ -267,7 +276,8 @@ class FixedTargetGAE(pl.LightningModule):
             device=args.device,
             fixed_init_coord=True,
             structured_seed=self.structured_seed,
-            anchor_feat=self.anchor_feat if self.structured_seed else None)
+            anchor_feat=self.anchor_feat if self.structured_seed else None,
+            anchor_coords=self.anchor_coords if self.structured_seed else None)
 
         self.register_buffer('init_coord', self.pool.init_coord.clone())
         self.mse = nn.MSELoss(reduction='none')
@@ -293,17 +303,27 @@ class FixedTargetGAE(pl.LightningModule):
         final_coord, final_node_feat = self.encoder(
             edge_index, init_coord, init_node_feat, n_steps=n_steps, n_nodes=batch.n_nodes)
 
-        edge_weight = torch.norm(final_coord[batch.rand_edge_index[0]] - final_coord[batch.rand_edge_index[1]], dim=-1)
-        loss_per_edge = self.mse(edge_weight, batch.rand_edge_weight)
-        loss_per_graph = torch.stack([lpe.mean() for lpe in loss_per_edge.chunk(batch_size)])
-        old_loss = loss_per_graph.mean()
-        
-        dist_loss, angle_loss = local_loss(final_coord, self.target_coord, edge_index)
-        loss = dist_loss + angle_loss
+        if self.loss_fn == 'mse':
+            edge_weight = torch.norm(final_coord[batch.rand_edge_index[0]] - final_coord[batch.rand_edge_index[1]], dim=-1)
+            loss_per_edge = self.mse(edge_weight, batch.rand_edge_weight)
+            loss_per_graph = torch.stack([lpe.mean() for lpe in loss_per_edge.chunk(batch_size)])
+            loss = loss_per_graph.mean()
+            loss_log = f'{loss:.6f}'
+        elif self.loss_fn == 'local':
+            with torch.no_grad():
+                edge_weight = torch.norm(final_coord[batch.rand_edge_index[0]] - final_coord[batch.rand_edge_index[1]], dim=-1)
+                loss_per_edge = self.mse(edge_weight, batch.rand_edge_weight)
+                loss_per_graph = torch.stack([lpe.mean() for lpe in loss_per_edge.chunk(batch_size)])
+                old_loss = loss_per_graph.mean()
+            dist_loss, angle_loss = local_loss(final_coord, self.target_coord, edge_index)
+            loss = dist_loss + angle_loss
+            loss_log = f'{dist_loss:.6f} + {angle_loss:.6f} = {loss:.6f} ({old_loss:.6f})'
+        else:
+            raise ValueError('Invalid loss function')
 
         # display & log
-        print('%d \t %.6f + %.6f = %.6f (%.6f) \t %d \t %.6f \t %.2f' %
-              (self.current_epoch, dist_loss, angle_loss, loss, old_loss, batch_size,
+        print('%d \t %s \t %d \t %.6f \t %.2f' %
+              (self.current_epoch, loss_log, batch_size,
                self.trainer.optimizers[0].param_groups[0]['lr'], self.pool.avg_reps))
         self.log('loss', loss, on_step=True, on_epoch=False, batch_size=batch_size)
         return loss

pools.py
@@ -130,6 +130,7 @@ class GaussianSeedPool:
         device: Optional[str] = 'cuda',
         structured_seed: Optional[bool] = False,
         anchor_feat: Optional[torch.Tensor] = None,
+        anchor_coords: Optional[torch.Tensor] = None
     ):
         assert std > 0.0
         assert std_damage >= 0
@@ -155,12 +156,9 @@ class GaussianSeedPool:
         if structured_seed:
             assert anchor_feat is not None
             self.anchor_feat = anchor_feat
-            assert node_dim >= coord_dim + 1
             if fixed_init_coord:
-                # Initialze coords of anchor nodes
-                anchor_coords = get_anchor_coords(coord_dim)
-                # Replace the last coord_dim+1 coords with the anchor coords.
-                self.init_coord[-(coord_dim+1):] = anchor_coords
+                # Replace the last coords with the anchor coords.
+                self.init_coord[-(anchor_coords.size(0)):] = anchor_coords
         self.pool_node_feat = self.init_node_feat.clone().unsqueeze(0).repeat(pool_size, 1, 1)
 
         self.pool_loss = torch.full((pool_size, ), torch.inf)
@@ -191,7 +189,7 @@ class GaussianSeedPool:
         else:
             coord[id_reset] = self.init_coord.clone().to(self.device)
         if self.structured_seed:
-            node_feat[:, -(self.coord_dim+1):] = self.anchor_feat
+            node_feat[:, -(self.anchor_feat.size(0)):] = self.anchor_feat
 
         if self.sparse:
             coord = coord.view(-1, self.coord_dim)

utils/utils.py
@@ -286,14 +286,27 @@ def compute_edge_index(
     return EdgeIndex(edge_index)
 
 def get_anchor_coords(
+    anchor_structure: str,
     coord_dim: int,
 ):
-    if coord_dim == 2:
-        # Add anchor nodes: equilateral triangle
-        return torch.tensor([[1, 0], [-0.5, 0.5 * 3 ** 0.5], [-0.5, -0.5 * 3 ** 0.5]])
+    if anchor_structure is None:
+        return None
+    elif anchor_structure == 'simplex':
+        if coord_dim == 2:
+            # Add anchor nodes: equilateral triangle
+            return torch.tensor([[1, 0], [-0.5, 0.5 * 3 ** 0.5], [-0.5, -0.5 * 3 ** 0.5]])
+        else:
+            # Add anchor nodes: tetrahedron
+            return torch.tensor([[1, 0, -0.5 * 2 ** 0.5], [-1, 0, -0.5 * 2 ** 0.5], [0, 1, 0.5 * 2 ** 0.5], [0, -1, 0.5 * 2 ** 0.5]])
+    elif anchor_structure == 'corners':
+        if coord_dim == 2:
+            return torch.Tensor([[1, 1], [1, -1], [-1, 1], [-1, -1]])
+        elif coord_dim == 3:
+            return torch.Tensor([[1, 1, 1], [1, -1, 1], [-1, 1, 1], [-1, -1, 1], [1, 1, -1], [1, -1, -1], [-1, 1, -1], [-1, -1, -1]])
+        else:
+            raise NotImplementedError("The corners anchor structure is only implemented for 2D and 3D coordinates")
     else:
-        # Add anchor nodes: tetrahedron
-        return torch.tensor([[1, 0, -0.5 * 2 ** 0.5], [-1, 0, -0.5 * 2 ** 0.5], [0, 1, 0.5 * 2 ** 0.5], [0, -1, 0.5 * 2 ** 0.5]])
+        raise ValueError("Invalid anchor structure. Must be either 'simplex' or 'corners'")
 
 # Adapted from https://github.com/pyg-team/pytorch_geometric/blob/caf5f57bf10f9b697b418ea7ec50594ee7a21b73/torch_geometric/nn/models/dimenet.py#L413-L433
 def triplets(
@@ -334,10 +347,14 @@ def calculate_angles(
 
     coord_ik, coord_ij = coord.index_select(0, idx_k) - coord.index_select(0, idx_i), coord.index_select(0, idx_j) - coord.index_select(0, idx_i)
 
-    dot_product = (coord_ij * coord_ik).sum(dim=-1)
-    norm_product = (coord_ij.norm(dim=-1) * coord_ik.norm(dim=-1)).clamp(min=1e-5)
-    eps = torch.tensor(1e-7, device=coord.device)
-    angle = torch.arccos((dot_product / (norm_product)).clamp(-1 + eps, 1 - eps)) / torch.pi
+    # dot_product = (coord_ij * coord_ik).sum(dim=-1)
+    # norm_product = (coord_ij.norm(dim=-1) * coord_ik.norm(dim=-1)).clamp(min=1e-5)
+    # eps = torch.tensor(1e-7, device=coord.device)
+    # angle = torch.arccos((dot_product / (norm_product)).clamp(-1 + eps, 1 - eps)) / torch.pi
+    angle = 2 * torch.atan2(
+        torch.norm(coord_ik * coord_ij, dim=1),
+        torch.norm(coord_ik, dim=1) * torch.norm(coord_ij, dim=1)
+    ) / torch.pi
     
     if return_indices:
         return angle, (idx_i, idx_j, idx_k)